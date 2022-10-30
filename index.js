9a0bc341c808cf83c84f648d3f9d1ee93f031019
Danushka-Madushan <Danushkamadushan128@gmail.com>
2022-10-31 01:34:02 +0530

    MControl initial commit

---
+++ index.js
@@ -0,0 +1,1 @@
+document.getElementById("strt").onclick = function(){}
---
+++ terminal.py
@@ -0,0 +1,12 @@
+# Make a Web interface that can control users pc with buttons and commands
+# Ex: there is a button called Shutdown When you pushed that button yout pc will shut down
+# Requirements : Pc and the mobile device must be in same network
+
+# Features
+	# Shutdown
+	# Sleep
+	# Sleep ++
+	# Take a Screenshot of PC
+	# Run Specific Command
+	
+# Login System
