2c9d3a198da55f69fcf2c24f7717aaf060ce4950
Danushka-Madushan <Danushkamadushan128@gmail.com>
2022-10-31 01:38:08 +0530

    initial templates for project

--- terminal.py
+++ terminal.py
@@ -1,3 +1,7 @@
+from flask import Flask
+from extenstinos import controls
+
+app = Flask(__name__)
 # Make a Web interface that can control users pc with buttons and commands
 # Ex: there is a button called Shutdown When you pushed that button yout pc will shut down
 # Requirements : Pc and the mobile device must be in same network
@@ -10,3 +14,6 @@
 	# Run Specific Command
 	
 # Login System
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
