2bba903f03e52194a35bc6195ec97811c4913546
Danushka-Madushan <Danushkamadushan128@gmail.com>
2022-12-16 07:59:04 +0530

    Used JQuery instead of JavaScript to Manipulate DOM

--- static/js/index.js
+++ static/js/index.js
@@ -1,26 +1,43 @@
-document.getElementById("caps").onclick = function() {
+$("#caps").click(function() {
     ExecutePC("ss").then(data => {
+        if (!$('.hidable-1').is(':visible')) {
+            //$(".hidable-1").show();
+            $(".hidable-1").fadeIn(400);
+        };
         $(document).ready(function() {
             $(".list").append($(`<span>`).html(`<a href="/media/${data['info'].media_id}/123456789" target="_blank"> Screenshot - ${data['info'].stamp}</a>`));
         });
     });
-};
+});
 
-document.getElementById("clr").onclick = function(){
+$("#clr").click(function() {
     ExecutePC("ss-clear").then(data => {
-        console.log(data)
         $(".list").empty()
+        if ($(".hidable-1").is(':visible')) {
+            //$(".hidable-1").hide();
+            $(".hidable-1").fadeOut(400);
+        };
     });
-};
+});
 
-document.getElementById("sdp").onclick = function(){
+$("#sdp").click(function() {
     ExecutePC("os-shut").then(data => {
-        alert(JSON.stringify(data, null, 4))
+        if (data['status'] == 'success') {
+            if (!$('.hidable-2').is(':visible')) {
+                $('.hidable-2').fadeIn(400);
+            };
+            //alert(JSON.stringify(data, null, 4))
+        };
     });
-};
+});
 
-document.getElementById("asdp").onclick = function(){
+$("#asdp").click(function() {
     ExecutePC("abort-shut").then(data => {
-        alert(JSON.stringify(data, null, 4))
+        if (data['status'] == 'success') {
+            if ($('.hidable-2').is(':visible')) {
+                $('.hidable-2').fadeOut(400);
+            };
+            //alert(JSON.stringify(data, null, 4))
+        };
     });
-};
+});
